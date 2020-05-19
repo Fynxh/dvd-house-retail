ec8c18b316bbee86124244850437981e5784fd1a
Fahmi Nuralamsah <nuralamsah.fahmi@gmail.com>
2020-05-19 16:57:42 +0700

    dvd-house-retail

---
+++ client/src/actions/types.js
@@ -0,0 +1,34 @@
+// for user
+export const USER_LOADING = 'USER_LOADING'
+export const USER_LOADED = 'USER_LOADED'
+
+// for barang
+export const BARANG_LOADING = 'BARANG_LOADING'
+export const GET_BARANG = 'GET_BARANG'
+export const ADD_BARANG = 'ADD_BARANG'
+export const DELETE_BARANGA = 'DELETE_BARANGA'
+export const UPDATE_BARANG = 'UPDATE_BARANG'
+
+// for keuangan
+export const KEUANGAN_LOADING = 'KEUANGAN_LOADING'
+export const GET_KEUANGAN = 'GET_KEUANGAN'
+export const ADD_KEUANGAN = 'ADD_KEUANGAN'
+export const DELETE_KEUANGAN = 'DELETE_KEUANGAN'
+export const UPDATE_KEUANGAN = 'UPDATE_KEUANGAN'
+
+// for laporan
+export const LAPORAN_LOADING = 'LAPORAN_LOADING'
+export const GET_LAPORAN = 'GET_LAPORAN'
+export const ADD_LAPORAN = 'ADD_LAPORAN'
+export const DELETE_LAPORAN = 'DELETE_LAPORAN'
+export const UPDATE_KEUANGAN = 'UPDATE_KEUANGAN'
+
+// for error
+export const GET_ERRORS = 'GET_ERRORS'
+export const CLEAR_ERRORS = 'CLEAR_ERRORS'
+
+// for auth
+export const AUTH_ERROR = 'AUTH_ERROR'
+export const LOGIN_SUCCESS = 'LOGIN_SUCCESS'
+export const LOGIN_FAIL = 'LOGIN_FAIL'
+export const LOGOUT_SUCCESS = 'LOGOUT_SUCCESS'
