594e9009709b49e9405900e6c209bf14d6da7e73
Fahmi Nuralamsah <nuralamsah.fahmi@gmail.com>
2020-05-22 20:41:54 +0700

    dvd-house-retail

---
+++ client/src/layout/DefaultHeader.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import { UncontrolledDropdown, DropdownItem, DropdownMenu, DropdownToggle, Nav, NavItem } from 'reactstrap';
+
+import { AppSidebarToggler } from '@coreui/react';
+
+// const propTypes = {
+//   children: PropTypes.node,
+// };
+
+// const defaultProps = {};
+
+class DefaultHeader extends Component {
+  render() {
+
+    // eslint-disable-next-line
+    // const { children, ...attributes } = this.props;
+
+    return (
+      <React.Fragment>
+        <AppSidebarToggler className="d-lg-none" display="md" mobile />
+        <AppSidebarToggler className="d-md-down-none" display="lg" />
+
+        <Nav className="d-md-down-none" navbar>
+          <NavItem className="px-3">
+            <NavLink to="/dashboard" className="nav-link" >Dashboard</NavLink>
+          </NavItem>
+          <NavItem className="px-3">
+            <NavLink to="#" className="nav-link">Settings</NavLink>
+          </NavItem>
+        </Nav>
+        <Nav className="ml-auto" navbar>
+          <UncontrolledDropdown nav direction="down">
+            <DropdownToggle nav>
+              <h4>Admin</h4>
+              {/* <img src={'../../assets/img/avatars/6.jpg'} className="img-avatar" alt="admin@bootstrapmaster.com" /> */}
+            </DropdownToggle>
+            <DropdownMenu right>
+              <DropdownItem onClick={this.props.onLogout}><i className="fa fa-lock"></i> Logout</DropdownItem>
+            </DropdownMenu>
+          </UncontrolledDropdown>
+        </Nav>
+        {/* <AppAsideToggler className="d-md-down-none" /> */}
+        {/*<AppAsideToggler className="d-lg-none" mobile />*/}
+      </React.Fragment>
+    );
+  }
+}
+
+// DefaultHeader.propTypes = propTypes;
+// DefaultHeader.defaultProps = defaultProps;
+
+export default DefaultHeader;
